db40135fadd0891389f537af7a5dc55e8922aa91
Liza Ituze <lizaituze1@gmail.com>
2026-05-10 23:11:07 +0200

    Render tasks dynamically on the UI

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const taskForm = document.getElementById("taskForm");
 const taskInput = document.getElementById("taskInput");
 const dateInput = document.getElementById("dateInput");
 const errorMessage = document.getElementById("errorMessage");
+const taskContainer = document.getElementById("taskContainer");
 
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -9,10 +10,32 @@ taskForm.addEventListener("submit", (e) => {
   const taskName = taskInput.value.trim();
   const dueDate = dateInput.value;
 
+  // Validation
   if (taskName === "" || dueDate === "") {
     errorMessage.classList.remove("hidden");
     return;
   }
 
   errorMessage.classList.add("hidden");
+
+  // Create Task Card
+  const taskCard = document.createElement("div");
+
+  taskCard.className =
+    "bg-white p-5 rounded-xl shadow-md border border-gray-200";
+
+  taskCard.innerHTML = `
+    <h4 class="text-xl font-bold mb-2">${taskName}</h4>
+
+    <p class="text-gray-600">
+      Due Date: ${dueDate}
+    </p>
+  `;
+
+  // Add card to screen
+  taskContainer.appendChild(taskCard);
+
+  // Clear Inputs
+  taskInput.value = "";
+  dateInput.value = "";
 });
