1ef5d6df4ac21583d98e925c5b7fc00d42809d2a
Liza Ituze <lizaituze1@gmail.com>
2026-05-11 11:01:29 +0200

    I added local storage support

--- script.js
+++ script.js
@@ -3,18 +3,58 @@ const taskInput = document.getElementById("taskInput");
 const dateInput = document.getElementById("dateInput");
 const errorMessage = document.getElementById("errorMessage");
 const taskContainer = document.getElementById("taskContainer");
+
+// Get saved tasks from local storage
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// Save tasks to local storage
 function saveTasks() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+// Show tasks on the page
+function renderTasks() {
+  taskContainer.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const taskCard = document.createElement("div");
+
+    taskCard.className =
+      "bg-white p-5 rounded-xl shadow-md border border-gray-200";
+
+    taskCard.innerHTML = `
+      <h4 class="text-xl font-bold mb-2">${task.name}</h4>
+
+      <p class="text-gray-600 mb-4">
+        Due Date: ${task.date}
+      </p>
+
+      <button
+        class="deleteBtn bg-red-500 hover:bg-red-600 transition text-white px-4 py-2 rounded-lg"
+      >
+        Delete
+      </button>
+    `;
+
+    const deleteBtn = taskCard.querySelector(".deleteBtn");
+
+    deleteBtn.addEventListener("click", () => {
+      tasks.splice(index, 1);
+      saveTasks();
+      renderTasks();
+    });
+
+    taskContainer.appendChild(taskCard);
+  });
+}
+
+// Add new task
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const taskName = taskInput.value.trim();
   const dueDate = dateInput.value;
 
-  // Validation
   if (taskName === "" || dueDate === "") {
     errorMessage.classList.remove("hidden");
     return;
@@ -22,34 +62,18 @@ taskForm.addEventListener("submit", (e) => {
 
   errorMessage.classList.add("hidden");
 
-  // Create Task Card
-  const taskCard = document.createElement("div");
+  const newTask = {
+    name: taskName,
+    date: dueDate,
+  };
 
-  taskCard.className =
-    "bg-white p-5 rounded-xl shadow-md border border-gray-200";
+  tasks.push(newTask);
+  saveTasks();
+  renderTasks();
 
-  taskCard.innerHTML = `
-  <h4 class="text-xl font-bold mb-2">${taskName}</h4>
-
-  <p class="text-gray-600 mb-4">
-    Due Date: ${dueDate}
-  </p>
-
-  <button
-    class="deleteBtn bg-red-500 hover:bg-red-600 transition text-white px-4 py-2 rounded-lg"
-  >
-    Delete
-  </button>
-`;
-const deleteBtn = taskCard.querySelector(".deleteBtn");
-
-deleteBtn.addEventListener("click", () => {
-  taskCard.remove();
-});
-  // Add card to screen
-  taskContainer.appendChild(taskCard);
-
-  // Clear Inputs
   taskInput.value = "";
   dateInput.value = "";
 });
+
+// Load saved tasks when page opens
+renderTasks();
